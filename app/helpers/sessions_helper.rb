93fbc7d737a54bcd4c1179a1b0f21eeefca9339d
Ryan <ryanmccombe@gmail.com>
2015-01-17 15:02:39 +0000

    do up to section 9.3

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -19,7 +19,10 @@ module SessionsHelper
         @current_user = user
       end
     end
-   # @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def current_user?(user)
+    user == current_user
   end
 
   def logged_in?
@@ -36,5 +39,14 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
   end
 end
