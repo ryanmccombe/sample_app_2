79b928fb01b340a7b421e97bf7443d2b95f0bb7c
Ryan <ryanmccombe@gmail.com>
2015-01-19 23:31:53 +0000

    Finish up to 11.3

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 User.create!(name: "Example User",
-             email: "foo@bar.com",
+             email: "example@railstutorial.org",
              password: "foobar",
              password_confirmation: "foobar",
              admin: true,
@@ -8,7 +8,7 @@ User.create!(name: "Example User",
 
 99.times do |n|
   name = Faker::Name.name
-  email = "example-#{n+1}@example.com"
+  email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name: name,
                email: email,
@@ -16,4 +16,10 @@ User.create!(name: "Example User",
                password_confirmation: password,
                activated: true,
                activated_at: Time.zone.now)
+end
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -62,4 +62,12 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.authenticated?(:remember, '')
   end
 
+  test "associated microposts should be destroyed with user" do
+    @user.save
+    @user.microposts.create!(content: "Lorum ipsum")
+    assert_difference "Micropost.count", -1 do
+      @user.destroy
+    end
+  end
+
 end
